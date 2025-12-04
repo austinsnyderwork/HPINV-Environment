933cb86bdfd392a7dcf9d4e45c7623c99d61929d
austinsnyderwork <179244141+austinsnyderwork@users.noreply.github.com>
2025-12-04 11:45:37 -0600

    c

--- src/hpinv_environment/entities/worksite.py
+++ src/hpinv_environment/entities/worksite.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Iterable
 
 import hpinv_enums
 import pandas as pd
@@ -15,27 +16,26 @@ class Worksite:
                  transaction_id: hpinv_enums.TransactionId = None,
                  address_1: str = None,
                  address_2: str = None,
-                 additional_attributes: dict = None):
+                 additional_attributes: dict = None,
+                 **kwargs):
         self.exists_in_hpinv = exists_in_hpinv
 
         self.worksite_id = int(worksite_id) if worksite_id and not pd.isna(worksite_id) else None
-        setattr(self, WorksiteColumn.WORKSITE_ID.value, worksite_id)
         self.parent_worksite_id = int(parent_worksite_id) if parent_worksite_id and not pd.isna(parent_worksite_id) else None
-        setattr(self, WorksiteColumn.PARENT_WORKSITE_ID.value, parent_worksite_id)
 
         self.transaction_id = transaction_id
-        setattr(self, WorksiteColumn.ACTIVE_STATUS.value, transaction_id)
 
         self.address_1 = address_1
-        setattr(self, WorksiteColumn.ADDRESS_1.value, address_1)
         self.address_2 = address_2
-        setattr(self, WorksiteColumn.ADDRESS_2.value, address_2)
 
-        if additional_attributes:
-            for k, v in additional_attributes.items():
+        if additional_attributes or kwargs:
+            additional_attributes = additional_attributes or dict()
+            kwargs = kwargs or dict()
+            d = additional_attributes | kwargs
+            for k, v in d.items():
                 setattr(self, k, v)
 
-        self.provider_ids = set()
+        self.provider_ids = set(kwargs['provider_ids']) or set()
 
     def __key(self):
         return self.worksite_id
@@ -58,6 +58,27 @@ class Worksite:
 
     @property
     def full_address(self) -> str:
-        parts = [self.address_1, self.address_2]
+        street_parts = [self.address_1, self.address_2]
+        street = " ".join(str(part).strip() for part in street_parts if part and str(part).strip())
 
-        return " ".join(part for part in parts if part and part.strip())
+        city = getattr(self, WorksiteColumn.CITY.value, "")
+        state = getattr(self, WorksiteColumn.STATE.value, "")
+        zip_code = getattr(self, WorksiteColumn.ZIP.value, "")
+        country = "USA"
+
+        comma_parts = [city, state, zip_code, country]
+
+        # Convert everything to string just in case
+        full = ", ".join(str(part) for part in [street] + comma_parts if part and str(part).strip())
+        return full
+
+    def to_dict(self):
+        d = self.__dict__
+        for k, v in d.items():
+            if isinstance(v, Iterable) and not isinstance(v, list):
+                d[k] = list(v)
+        return d
+
+    @classmethod
+    def from_dict(cls, d: dict) -> "Worksite":
+        return Worksite(**d)
